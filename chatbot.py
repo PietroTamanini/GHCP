8e77290d922c16d23d3ac3534c475441e228325a
PietroTamanini <pietroramostamanini2010@gmail.com>
2025-11-14 13:35:29 -0300

    Adição do bot com alguns erros
    marcas.html "completa"

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,4 @@
 respostas = {
-    "como cadastrar": "Para cadastrar um item, vá no menu 'Cadastro' e clique em 'Novo'.",
     "esqueci a senha": "Clique na opção 'Recuperar Senha' na tela de login.",
     "erro 404": "Isso significa que a página solicitada não foi encontrada.",
     "como excluir": "Vá na listagem, selecione o item e clique no botão 'Excluir'."
